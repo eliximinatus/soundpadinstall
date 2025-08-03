f089d69f8149d5fea72f9505a1355028abee5f18
eli <50389389+eliximinatus@users.noreply.github.com>
2025-08-03 12:35:53 +0300

    Added README and the ACTUAL script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# soundpadinstall
-Automatically downloads and installs sounds/videos to Soundpad, and cleans up after itself
+This is a simple, lightweight Python script to automate downloading sound files from YouTube, BiliBili, and any other supported sites, and installing them to Soundpad.
+# Explanation
+
+This script downloads the file with the prefix "SOUNDPADINSTALL.", looks for each and every file with that same prefix in the current directory, adds it to a list, hosts the directory on localhost, then runs "soundpad://sound/url/https://localhost:45993/FileName
+# Dependencies
+You need [ffmpeg + ffprobe](https://ffmpeg.org/), and [yt_dlp](https://github.com/yt-dlp/yt-dlp)
+
+Run `pip install yt_dlp` in your terminal, or `./bin/pip install yt_dlp` if you're using a VENV
+
+ffmpeg can be installed through your distro's package manager, or through the links provided above.
+
+
+
+
---
+++ yt-to-soundpad.py
@@ -0,0 +1,65 @@
+import os
+import yt_dlp
+import argparse
+import webbrowser
+import time
+
+parser = argparse.ArgumentParser(description="Hi")
+parser.add_argument("url", type=str, default='N/A', help="The link to the video (or sound) you'd like to download", nargs=1)
+
+
+args = parser.parse_args()
+url = args.url
+if isinstance(url, list):
+    url = url[0]
+
+if url ==  "N/A":
+    print('Please enter a URL!')
+    exit()
+
+def download_video(link):
+    ytopt = {
+        'outtmpl': f'./SOUNDPADINSTALL.%(title)s.%(ext)s',
+        'format': 'bestaudio',
+        'quiet': True,          
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",  # Use FFmpeg to extract audio
+                "preferredcodec": "mp3",     # Convert to MP3
+                "preferredquality": "192",   # Set desired audio quality (e.g., 192kbps)
+            }
+        ],
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ytopt) as ytdl:
+            print('Attempting download...')
+            ytdl.download([link])
+        print('Downloaded!')
+
+    except Exception as error:
+        print(f"Downloading \"{link}\" failed: {error}")
+        exit()
+
+download_video(url)
+
+dir = os.listdir()
+
+deathmark = []
+
+for file in dir:
+    if file.startswith('SOUNDPADINSTALL.'):
+        os.rename(file, file.removeprefix('SOUNDPADINSTALL.'))
+        time.sleep(1)
+        print(f'Found {file}')
+        deathmark.append(file.removeprefix('SOUNDPADINSTALL.'))
+        
+for markedfordeath in deathmark:
+    webbrowser.open(f'soundpad://sound/url/https://localhost:45993/{markedfordeath}')
+    time.sleep(3)
+    os.remove(markedfordeath)
+
+print('Done!')
+print('Exiting...')
+
+
